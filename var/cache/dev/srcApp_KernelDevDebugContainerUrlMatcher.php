60b2e0ea95e37df1672a8f3e40978ae9a2a3e397
TheoLedour <theoledourr@gmail.com>
2019-05-28 11:19:57 +0200

    TEST

---
+++ var/cache/dev/srcApp_KernelDevDebugContainerUrlMatcher.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\Routing\Matcher\Dumper\PhpMatcherTrait;
+use Symfony\Component\Routing\RequestContext;
+
+/**
+ * This class has been auto-generated
+ * by the Symfony Routing Component.
+ */
+class srcApp_KernelDevDebugContainerUrlMatcher extends Symfony\Bundle\FrameworkBundle\Routing\RedirectableUrlMatcher
+{
+    use PhpMatcherTrait;
+
+    public function __construct(RequestContext $context)
+    {
+        $this->context = $context;
+        $this->staticRoutes = [
+            '/number' => [[['_route' => 'app_lucky_number', '_controller' => 'App\\Controller\\LuckyController::number'], null, null, null, false, false, null]],
+        ];
+    }
+}
